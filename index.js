fd51ee991a10d216f3574d9d101fab9824eec405
devesh1808 <devesh1808@gmail.com>
2024-12-06 02:12:21 +0530

    used writeFile method from file syatem(fs) native module

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello from Node!");
+const fs = require("fs");
+
+fs.writeFile("message.txt", "Hello from NodeJs!", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
